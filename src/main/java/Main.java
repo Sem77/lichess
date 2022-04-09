9c3c686fbb268def1ca1d8ea77362c6dabecb5c4
sem <semwong77@gmail.com>
2022-04-10 00:34:04 +0200

    refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,121 +13,12 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String args[]) throws IOException, ClassNotFoundException, ParseException, NoFileDataFoundException, NoSuchMethodException {
 
-        String pgn01 = "/home/ubuntu/IdeaProjects/database/data_src/lichess_db_standard_rated_2013-01.pgn";
-        String pgn02 = "/home/ubuntu/IdeaProjects/database/data_src/lichess_db_standard_rated_2013-02.pgn";
-        String pgn03 = "/home/ubuntu/IdeaProjects/database/data_src/lichess_db_standard_rated_2013-03.pgn";
-        String pgn04 = "/home/ubuntu/IdeaProjects/database/data_src/lichess_db_standard_rated_2013-04.pgn";
-        String pgn05 = "/home/ubuntu/IdeaProjects/database/data_src/lichess_db_standard_rated_2013-05.pgn";
-        String pgn06 = "/home/ubuntu/IdeaProjects/database/data_src/lichess_db_standard_rated_2013-06.pgn";
-        String pgn07 = "/home/ubuntu/IdeaProjects/database/data_src/lichess_db_standard_rated_2013-07.pgn";
-
-        //ArrayList<String> pgnPaths = new ArrayList<>();
-        //pgnPaths.add(pgn01); pgnPaths.add(pgn02); pgnPaths.add(pgn03); pgnPaths.add(pgn04); pgnPaths.add(pgn05); pgnPaths.add(pgn06); pgnPaths.add(pgn07);
-
-        OptimizationWriter ow = new OptimizationWriter();
-        //ow.saveOptimizedGames(pgn01, "01", "2013", 4);
-        //ow.saveOptimizedGames(pgn02, "02", "2013", 4);
-        //ow.saveOptimizedGames(pgn03, "03", "2013", 4);
-        //ow.saveOptimizedGames(pgn04, "04", "2013", 4);
-        //ow.saveOptimizedGames(pgn05, "05", "2013", 4);
-        //ow.saveOptimizedGames(pgn06, "06", "2013", 4);
-        //ow.saveOptimizedGames(pgn07, "07", "2013", 4);
-
-
-        AssociationPlayerGamesMonitor apg = new AssociationPlayerGamesMonitor("03", "2014");
-        MostActivePlayerMonitor mapom = new MostActivePlayerMonitor("02", "2013");
-        MostPlayedOpeningMonitor mpom = new MostPlayedOpeningMonitor("02", "2013");
-        ShortestGamesMonitor sgm = new ShortestGamesMonitor("02", "2013");
-        PlayerInfoWriter piw = new PlayerInfoWriter("02", "2013");
-        FindGameWithLink fgwl = new FindGameWithLink("02", "2013");
-
-
-        //apg.buildHastable();
-        //mapom.buildHastable();
-        //mpom.buildHastable();
-        //sgm.buildHastable();
-        //piw.buildHastable();
-        //fgwl.buildHastable();
-
-        //mpom.saveNMostPlayedOpening();
-        //mapom.saveNMostActivePlayers();
-
-
-        AssociationPlayerGamesYear apgy = new AssociationPlayerGamesYear("2013");
-        MostPlayedOpeningYear mpoy = new MostPlayedOpeningYear("2013");
-        MostActivePlayerYear mapy = new MostActivePlayerYear("2013");
-        ShortestGamesYear sgy = new ShortestGamesYear("2013");
-        PlayerInfoYear piy = new PlayerInfoYear("2013");
-        FindGameWithLinkYear fgwly = new FindGameWithLinkYear("2013");
-
-        //apgy.buildHashtable();
-        //mpoy.buildHashtable();
-        //mapy.buildHashtable();
-        //sgy.buildHashtable();
-        //piy.buildHashtable();
-        //fgwly.buildHashtable();
-
-        //mpoy.saveNMostPlayedOpening();
-        //mapy.saveNMostActivePlayers();
-
-
-
-        MostPlayedOpeningAll mpoa = new MostPlayedOpeningAll();
-        ShortestGamesAll sga = new ShortestGamesAll();
-        AssociationPlayerGamesAll apga = new AssociationPlayerGamesAll();
-        MostActivePlayerAll mapa = new MostActivePlayerAll();
-        PlayerInfoAll pia = new PlayerInfoAll();
-        FindGameWithLinkAll fgwla = new FindGameWithLinkAll();
-
-
-        //mpoa.buildHashtable();
-        //sga.buildHashtable();
-        //apga.buildHashtable();
-        //mapa.buildHashtable();
-        //pia.buildHashtable();
-        //fgwla.buildHashtable();
-
-        //mpoa.saveNMostPlayedOpening();
-        //sga.saveFiveShortestGames();
-        //mapa.saveNMostActivePlayers();
-        //pia.saveBestPlayers();
-
-
-
-
-
-
-
-
         //buildHashtablesGamesOfAPlayer();
         //buildHashtablesMostPlayedOpening();
         //buildHashtablesMostActivePlayers();
         //buildHashtablesShortestGames();
         //buildHashtablesPlayerInfo();
-        buildHashtablesToFindGameWithLink();
-
-
-        /*ow.pageRankCalculator("/home/ubuntu/IdeaProjects/database/data_dest/games_data/2013/02/hashtables/players_info_hashtable.dat");
-
-        ObjectInputStream o = new ObjectInputStream(new FileInputStream("/home/ubuntu/IdeaProjects/database/data_dest/games_data/2013/02/hashtables/players_info_hashtable.dat"));
-
-        try {
-            Hashtable<String, Player> h = (Hashtable<String, Player>) o.readObject();
-            System.out.println(h);
-        } catch(EOFException eofe) {}
-        catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        o.close();*/
-
-        /*ObjectInputStream o = new ObjectInputStream(new FileInputStream("/home/ubuntu/IdeaProjects/database/data_dest/games_data/2013/game_link_over_a_year_hashtable.dat"));
-
-            Hashtable<String, String> h = (Hashtable<String, String>) o.readObject();
-            System.out.println(h.get("https://lichess.org/k2udryk3"));*/
-
-
-        //buildBinaryGames("/home/ubuntu/IdeaProjects/database/data_src/games_pgn");
-        //extract1000FirstGames("/home/ubuntu/IdeaProjects/database/data_src", "lichess_db_standard_rated_2014-02.pgn", "/home/ubuntu/IdeaProjects/database/data_src/games_pgn/2014/02");
+        //buildHashtablesToFindGameWithLink();
 
     }
 
